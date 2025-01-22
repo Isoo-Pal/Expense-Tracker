95aa48903e6e0cfe612fba10f4a9593583ef547b
Isoo Pal <17isoo12@gmail.com>
2025-01-22 22:16:29 +0530

    Adding Navbar

--- ETracker/ETracker/settings.py
+++ ETracker/ETracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ETracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ExpenseTrackerdb',
+        'USER': 'ISOO PAL',
+        'HOST': 'localhost',
+        'PASSWORD':'',
+        'PORT': 3306,
     }
 }
 
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # If you have a global static folder at the root
+]
